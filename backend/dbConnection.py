83a0de8f7aee43afe9223668560bdaa1697f04f0
jdivass <julianandre.divas@gmail.com>
2026-05-02 22:54:23 -0600

    Added dockerized backend

--- backend/dbConnection.py
+++ backend/dbConnection.py
@@ -1,21 +1,16 @@
 import psycopg2 as psy
 import os
-from dotenv import dotenv_values
 
 def db_connection():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    env_path = os.path.normpath(os.path.join(base_dir, "..", ".env"))
-
-    config = dotenv_values(env_path, encoding="utf-8")
-
     try:
         connection = psy.connect(
-            dbname=config.get("DB_NAME"),
-            user=config.get("DB_USER"),
-            password=config.get("DB_PASSWORD"),
-            host=config.get("DB_HOST"),
-            port=config.get("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
         )
+        print("6 - conexión exitosa")
         return connection
     except Exception as error:
         print("7 - error conexión:", error)
